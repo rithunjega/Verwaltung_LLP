fd0069472ea2003572c0400453137e1d96b41366
rithunjega <rithun.jegatheeswaran@gmail.com>
2021-09-06 15:09:52 +0200

    Parser Funktionsklasse erstellt

---
+++ src/application/Parser.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Parser {
+    public Parser() {
+    }
+
+    public static String parse(String needToSplit) {
+        String[] Array = needToSplit.split(",");
+        int sum = 0;
+
+        for(int i = 0; i < Array.length; ++i) {
+            int x = Integer.parseInt(Array[i]);
+            sum += x;
+        }
+
+        return String.valueOf(sum);
+    }
+}
